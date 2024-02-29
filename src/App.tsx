283e4fefc2177d1f53e037e69253214b471299d8
Haswanth <Haswanth1999@gmail.com>
2024-02-29 10:41:35 -0500

    Build a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`
-    }}>
-      <GridItem area="nav" bg="coral">
-        nav
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
 
       <Show above="lg">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
-import { HStack, Image } from '@chakra-ui/react'
-import React from 'react'
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import { px } from "framer-motion";
 
 const NavBar = () => {
   return (
-    <HStack> 
-        <Image />
+    <HStack>
+      <Image src={logo} boxSize='60px' />
+      <Text>NavBar</Text>
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
